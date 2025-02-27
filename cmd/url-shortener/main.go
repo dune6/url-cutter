b74eb30da9e4d1374fa931e38ae52b7bde16b53d
dune6 <ilya.voznesensky@gmail.com>
2025-02-27 04:18:39 +0300

    setup logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"urlShortener/internal/config"
 )
 
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
+)
+
 func main() {
-
-	// todo init config: cleanenv
 	cfg := config.MustLoad()
-	fmt.Printf("%+v\n", cfg)
+	_ = cfg
 
-	// todo init logger: log/slog
+	log := setupLogger(cfg.Env)
+	log.Info("Logger has been initialized", slog.String("env", cfg.Env))
 
 	// todo init db: sqlite
 
@@ -19,4 +25,18 @@ func main() {
 
 	// todo run server
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
